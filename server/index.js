68ee35115ecd93d618c1be63035b5cfd1bb0d5c7
Derriel Collins <derrielcollins96@gmail.com>
2021-09-22 13:19:52 -0500

    installed body-parser, cookie-parser, express session, to save cookies and sessions for staying loged in

--- server/index.js
+++ server/index.js
@@ -4,7 +4,13 @@ const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
 //Below is the component that will help hash the passwords
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
 const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -20,21 +26,27 @@ const db = mysql.createConnection({
 });
 
 app.post('/register', (req, res)=> {
-	
-	//these request names need to match the names of the key on the front-end or client-side component, that is
-	//normally located in the App directory in my code
-	//right now the client side component is home, when i make the new
-	//register component the client side component will be in register folder and not in the home folder
 	const username = req.body.username;
 	const password = req.body.password;
 
-	db.query(
-		"INSERT INTO users (username, password) VALUES (?,?)",
-		[username, password],
-		(err, result) => {
-			console.log(err);
+
+
+	bcrypt.hash(password, saltRounds, (err, hash) => {
+		if (err) {
+			res.send({err: err});
 		}
-	);
+
+		db.query(
+			"INSERT INTO users (username, password) VALUES (?,?)",
+			[username, hash],
+			(err, result) => {
+				console.log(err);
+			}
+		);
+
+	});
+
+
 });
 
 app.post('/login', (req, res)=> {
@@ -42,17 +54,23 @@ app.post('/login', (req, res)=> {
 	const password = req.body.password;
 
 	db.query(
-		"SELECT * FROM users WHERE username = ? AND password = ?",
-		[username, password],
+		"SELECT * FROM users WHERE username = ?",
+		username,
 		(err, result) => {
 			if(err) {
-			  res.send({err: err})
-			}	
+			  res.send({err: err});
+			}
+
 			if (result.length > 0) {
-				//check wether result has a length of greater then zero, I think we have to do this becuase the result is a //or object that can exsist w/o being populated 
-				res.send(result);
+				bcrypt.compare(password, result[0].password, (error, response) => {
+					if (response) {
+						res.send(result);
+					} else {
+						res.send({ message: "wrong/username/password combo"})
+					}
+				});
 			} else {
-				res.send({ message: "Wrong username/password combination!"});
+				res.send({ message: "Wrong username does not exist"});
 			}
 		}
 	);
